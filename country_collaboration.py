a50b9f95c37c199a811f103b7bf8519cce9d7b5b
Caaminp <160285915+Caaminp@users.noreply.github.com>
2024-03-15 23:06:29 -0500

    intento 2

--- country_collaboration.py
+++ country_collaboration.py
@@ -1,7 +1,8 @@
 """Taller Presencial Evaluable"""
 
+import matplotlib.pyplot as plt
+import networkx as nx
 import pandas as pd
-import folium
 
 
 def load_affiliations():
@@ -23,7 +24,7 @@ def remove_na_rows(affiliations):
     return affiliations
 
 
-def add_countries_column(affiliations):
+def create_countries_column(affiliations):
     """Transforma la columna 'Affiliations' a una lista de paises."""
 
     affiliations = affiliations.copy()
@@ -41,15 +42,6 @@ def add_countries_column(affiliations):
     return affiliations
 
 
-def clean_countries(affiliations):
-
-    affiliations = affiliations.copy()
-    affiliations["countries"] = affiliations["countries"].str.replace(
-        "United States", "United States of America"
-    )
-    return affiliations
-
-
 def count_country_frequency(affiliations):
     """Cuenta la frecuencia de cada país en la columna 'countries'"""
 
@@ -57,39 +49,96 @@ def count_country_frequency(affiliations):
     countries = countries.str.split(", ")
     countries = countries.explode()
     countries = countries.value_counts()
+    #
+    countries.to_csv("countries.csv")
+    #
     return countries
 
 
-def plot_world_map(countries):
-    """Grafica un mapa mundial con la frecuencia de cada país."""
+def select_most_frequente_countries(countries, n_countries):
+    """Selecciona los n países más frecuentes"""
 
     countries = countries.copy()
-    countries = countries.to_frame()
-    countries = countries.reset_index()
-
-    m = folium.Map(location=[0, 0], zoom_start=2)
-
-    folium.Choropleth(
-        geo_data="https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/world-countries.json",
-        data=countries,
-        columns=["countries", "count"],
-        key_on="feature.properties.name",
-        fill_color="Greens",
-    ).add_to(m)
-
-    m.save("map.html")
+    countries = countries.head(n_countries)
+    return countries
 
 
-def main():
+def compute_co_occurrences(affiliations, most_frequent_countries):
+    """Cuenta la frecuencia de co-ocurrencia de los países más frecuentes"""
+
+    affiliations = affiliations.copy()
+    co_occurrences = affiliations[["countries"]].copy()
+    co_occurrences = co_occurrences.rename(columns={"countries": "node_a"})
+    co_occurrences["node_b"] = co_occurrences["node_a"]
+
+    co_occurrences["node_a"] = co_occurrences["node_a"].str.split(", ")
+    co_occurrences = co_occurrences.explode("node_a")
+    co_occurrences = co_occurrences[
+        co_occurrences["node_a"].isin(most_frequent_countries.index)
+    ]
+
+    co_occurrences["node_b"] = co_occurrences["node_b"].str.split(", ")
+    co_occurrences = co_occurrences.explode("node_b")
+    co_occurrences = co_occurrences[
+        co_occurrences["node_b"].isin(most_frequent_countries.index)
+    ]
+
+    co_occurrences = co_occurrences[
+        co_occurrences["node_a"] != co_occurrences["node_b"]
+    ]
+    co_occurrences = co_occurrences.groupby(["node_a", "node_b"], as_index=False).size()
+
+    #
+    co_occurrences.to_csv("co_occurrences.csv")
+    #
+
+    return co_occurrences
+
+
+def plot_country_collaboration(countries, co_occurrences):
+    """Grafica la red de co-occurrencias."""
+
+    G = nx.Graph()
+
+    for _, row in co_occurrences.iterrows():
+        G.add_edge(row["node_a"], row["node_b"], weight=row["size"])
+
+    pos = nx.spring_layout(G)
+
+    countries_list = list(G)
+
+    node_size = countries[countries_list].values
+
+    nx.draw(
+        G,
+        pos,
+        with_labels=False,
+        node_size=node_size,
+        node_color="grey",
+        edge_color="lightgrey",
+        font_size=8,
+        alpha=1.0,
+    )
+
+    for country in pos.keys():
+        x, y = pos[country]
+        plt.text(x, y, country, fontsize=7, ha="center", va="center")
+
+    plt.savefig("network.png")
+
+
+def main(n_countries):
     """Función principal"""
     affiliations = load_affiliations()
     affiliations = remove_na_rows(affiliations)
-    affiliations = add_countries_column(affiliations)
-    affiliations = clean_countries(affiliations)
-    countries = count_country_frequency(affiliations)
-    countries.to_csv("countries.csv")
-    plot_world_map(countries)
+    affiliations = create_countries_column(affiliations)
+    countries_frequency = count_country_frequency(affiliations)
+    most_frequent_countries = select_most_frequente_countries(
+        countries_frequency, n_countries
+    )
+    co_occurrences = compute_co_occurrences(affiliations, most_frequent_countries)
+    plot_country_collaboration(most_frequent_countries, co_occurrences)
 
 
 if __name__ == "__main__":
-    main()
+    main(n_countries=20)
